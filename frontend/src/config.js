0dcd2446cadbaa677b04c39c33fa5329bddf70b6
dileep123321 <dileepyadavpm@gmail.com>
2025-11-11 08:28:42 +0530

    first commit

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-const url = import.meta.env.VITE_BACKEND_URL || "http://35.225.48.45:8080";
-console.log("✅ BACKEND_URL:", url);
-export const BACKEND_URL = url;
+// src/config.js
+export const BACKEND_URL =
+  import.meta.env.VITE_BACKEND_URL || "http://35.225.48.45:8080";
